5750ddd1eab2870732afaa8b0b5fa33253686cff
gsborgz <borgesgabrielsilva@gmail.com>
2026-06-08 22:32:15 -0300

    adding speed variations (walking, jogging and sprinting)

--- src/player/PlayerMovementHandler.gd
+++ src/player/PlayerMovementHandler.gd
@@ -1,12 +1,12 @@
 class_name PlayerMovementHandler
 
-
-const WALK_SPEED: float = 5.0
-const SPRINT_SPEED: float = 8.0
+const WALKING_SPEED: float = 0.5
+const JOGGING_SPEED: float = 5.0
+const SPRINTING_SPEED: float = 8.0
 const JUMP_VELOCITY: float = 4.5
 const STAND_HEIGHT: float = 2.0
 const CROUCH_HEIGHT: float = 0.5
-const CROUCH_SPEED_MODIFIER: float = 0.3
+const CROUCH_SPEED_MODIFIER: float = 0.4
 const CROUCH_JUMP_MODIFIER: float = 0.8
 
 var character: CharacterBody3D
@@ -17,6 +17,7 @@ var speed: float
 var is_sprinting: bool = false
 var is_moving: bool = false
 var is_crouching: bool = false
+var is_walking: bool = false
 
 
 func _init(_character: CharacterBody3D, _head: Node3D, _camera: Camera3D, _collision_shape: CapsuleShape3D) -> void:
@@ -32,7 +33,7 @@ func handle_movement(delta: float) -> void:
 	
 	_add_gravity(delta)
 	_handle_jump()
-	_handle_sprint()
+	_handle_move_speed()
 	_handle_crouch()
 	_handle_movement(delta)
 	
@@ -59,12 +60,15 @@ func _handle_jump() -> void:
 		character.velocity.y = JUMP_VELOCITY * CROUCH_JUMP_MODIFIER if is_crouching else JUMP_VELOCITY
 
 
-func _handle_sprint() -> void:
-	if Input.is_action_pressed("sprint") and character.is_on_floor():
-		speed = SPRINT_SPEED * CROUCH_SPEED_MODIFIER if is_crouching else SPRINT_SPEED 
+func _handle_move_speed() -> void:
+	if Input.is_action_pressed("sprint") and character.is_on_floor() and !is_crouching:
+		speed = SPRINTING_SPEED 
 		is_sprinting = true
+	elif Input.is_action_pressed("walk") and character.is_on_floor():
+		speed = WALKING_SPEED
+		is_sprinting = false
 	else:
-		speed = WALK_SPEED * CROUCH_SPEED_MODIFIER if is_crouching else WALK_SPEED
+		speed = JOGGING_SPEED * CROUCH_SPEED_MODIFIER if is_crouching else JOGGING_SPEED
 		is_sprinting = false
 
 
